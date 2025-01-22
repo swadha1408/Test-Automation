b19c6fc393fcf98c2a2d1f89b1da72dc2c3cf641
swadha1408 <68777827+swadha1408@users.noreply.github.com>
2025-01-22 14:42:45 +0530

    Update Script1737536985273.groovy

--- Scripts/newtcgit/Script1737536985273.groovy
+++ Scripts/newtcgit/Script1737536985273.groovy
@@ -17,5 +17,5 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('login.salesforce.com')
+WebUI.openBrowser('www.login.salesforce.com')
 
